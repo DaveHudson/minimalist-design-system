983283c99cf79f74341ad5c29763a66a8c48a53e
Dave Hudson <dave@applificationhq.com>
2018-01-21 11:26:06 +0000

    test(Fixing test setup to run with proptypes):

--- src/headertext/HeaderText.js
+++ src/headertext/HeaderText.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class HeaderTextElement extends Component {
+  static propTypes = {
+    color: PropTypes.string.isRequired,
+    children: PropTypes.any,
+  };
+  static defaultProps = {
+    color: '#000000',
+  };
+
   render() {
     return (
       <p>Hello there!</p>
